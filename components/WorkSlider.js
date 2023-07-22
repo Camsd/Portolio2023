5401244514ef185ee7dd8820df543d993369f5ff
Camsd <codecos.info@gmail.com>
2023-07-21 20:32:09 -0500

    redireccionamiento de url con propiedad al objeto images

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -6,18 +6,22 @@ const workSlides = {
         {
           title: "title",
           path: "/thumb1.jpg",
+          link: "https://www.facebook.com",
         },
         {
           title: "title",
           path: "/thumb2.jpg",
+          link: "https://www.facebook.com",
         },
         {
           title: "title",
           path: "/thumb3.jpg",
+          link: "https://www.facebook.com",
         },
         {
           title: "title",
           path: "/thumb4.jpg",
+          link: "https://www.facebook.com",
         },
       ],
     },
@@ -26,18 +30,22 @@ const workSlides = {
         {
           title: "title",
           path: "/thumb4.jpg",
+          link: "https://www.facebook.com",
         },
         {
           title: "title",
           path: "/thumb1.jpg",
+          link: "https://www.facebook.com",
         },
         {
           title: "title",
           path: "/thumb2.jpg",
+          link: "https://www.facebook.com",
         },
         {
           title: "title",
           path: "/thumb3.jpg",
+          link: "https://www.facebook.com",
         },
       ],
     },
@@ -53,7 +61,7 @@ import "swiper/css/free-mode";
 import "swiper/css/pagination";
 
 //import icons
-import {BsArrowRight} from 'react-icons/bs';
+import { BsArrowRight } from "react-icons/bs";
 
 //import required modules
 import { Pagination } from "swiper";
@@ -77,15 +85,19 @@ const WorkSlider = () => {
             <div className="grid grid-cols-2 gap-4 cursor-pointer">
               {slide.images.map((image, index) => {
                 return (
-                  <div className="relative rounded-lg overflow-hidden flex items-center justify-center group" key={index}>
+                  <div
+                    className="relative rounded-lg overflow-hidden flex items-center justify-center group"
+                    key={index}
+                  >
+                    <a href={image.link} target="_blank" rel="noopener noreferrer">
                     <div className="flex items-center justify-center relative overflow-hidden group">
-                      {/* image */}
-                      <Image
-                        src={image.path}
-                        width={500}
-                        height={300}
-                        alt="projects"
-                      />
+                        {/* image */}
+                        <Image
+                          src={image.path}
+                          width={500}
+                          height={300}
+                          alt="projects"
+                        />
                       {/* overlay gradient */}
                       <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
                       {/* title */}
@@ -94,12 +106,17 @@ const WorkSlider = () => {
                           {/* title part 1 */}
                           <div className="delay-100">LIVE</div>
                           {/* title part 2 */}
-                          <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">PROJECT</div>
+                          <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
+                            PROJECT
+                          </div>
                           {/* icon */}
-                          <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200"><BsArrowRight /></div>
+                          <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
+                            <BsArrowRight />
+                          </div>
                         </div>
                       </div>
                     </div>
+                    </a>
                   </div>
                 );
               })}
